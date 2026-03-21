eb971206ea73fe18eee85318001ca0cdc0044b59
OskarLM <32792036+OskarLM@users.noreply.github.com>
2026-03-21 10:29:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — MIS GASTOS (GitHub Pages)
-const CACHE_VERSION = "v1.0.82";
+const CACHE_VERSION = "v1.0.83";
 const CACHE_NAME = `mis-gastos-${CACHE_VERSION}`;
 const BASE = "/APK_V0.0/";
 
